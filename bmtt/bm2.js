899f0cf9e87ed44e3218b7a6c517a96f375eb043
y.sato <y.sato@extia.jp>
2021-03-02 02:35:03 +0900

    Update bm2.js

--- bmtt/bm2.js
+++ bmtt/bm2.js
@@ -50,7 +50,7 @@ $css += '#exL .gs input[type="button"] { width:30px; height:30px; font-size:12px
 	}
 	let $a = d.querySelectorAll('.match-flex-0 .name'), $b = [], $c = '', $d = '', $t = '';
 	for(i=0; i<$a.length; i++){
-		var $name = $a[i].innerText:$a[i].children[0].value;
+		var $name = ($a[i].innerText)?$a[i].innerText:$a[i].children[0].value;
 		if($name.length){
 			$b.push($a[i].innerText);
 			$t += '<option>' + (($a[i].innerText)?$a[i].innerText:$a[i].children[0].value) + '</option>';
