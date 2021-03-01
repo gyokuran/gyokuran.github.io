2b6f7368d569d3f4a5217209b784b02c27f61ca6
y.sato <y.sato@extia.jp>
2021-03-02 02:44:51 +0900

    Update bm2.js

--- bmtt/bm2.js
+++ bmtt/bm2.js
@@ -78,7 +78,7 @@ $css += '#exL .gs input[type="button"] { width:30px; height:30px; font-size:12px
 	});
 
 })(document);
-if(typeof $_pImg == 'undefined'){
+if(typeof $_pImg !== 'function'){
 const $_pImg = function(obj){
 	let fileReader = new FileReader();
 	fileReader.onload = (function(){
