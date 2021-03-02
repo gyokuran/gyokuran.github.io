e1eb8d8bef53fb481d1f940d8401a81f2b1b5c83
y.sato <y.sato@extia.jp>
2021-03-02 10:39:30 +0900

    Update bm2.js

--- bmtt/bm2.js
+++ bmtt/bm2.js
@@ -47,7 +47,7 @@ $css += '#exL #exL_logo.set:before { display:none; }';
 $css += '#exL .gs { display:flex; justify-content:flex-end; padding:5px 5px 0 0; }';
 $css += '#exL .gs input[type="text"] { width:150px height:30px; font-size:16px; line-height:30px; padding:0 5px; border:1px solid #CCC; }';
 $css += '#exL .gs input[type="button"] { width:30px; height:30px; font-size:12px; border:1px solid #CCC; }';
-$css += '#exL #btn_switch { width:auto; height:auto; padding:4px; bottom:-26px; }';
+$css += '#exL #btn_switch { width:auto; height:auto; padding:4px; bottom:-26px; font-size:12px; }';
 
 	let $s = d.createElement('style'); $s.innerHTML = $css; d.body.prepend($s);
 	}
@@ -61,7 +61,7 @@ $css += '#exL #btn_switch { width:auto; height:auto; padding:4px; bottom:-26px; 
 	};
 	$d += '<div id="exL"><div class="wrap"><div class="inner">';
 	$d += '<div class="left"><div class="team"><select class="name saveobj" id="player_name_l"><option class="def">-リストを読み込んでください-</option></select><select class="team saveobj" id="player_team_l" onChange="$_setTeam(this)">'+$t+'</select></div><input type="number" value="0" class="num saveobj" id="player_score_l" maxlength="1" min="0" max="9"><span id="up_L" class="numbtn nubup">+</span><span id="down_L" class="numbtn nubclr">0</span></div>';
-	$d += '<div class="space"><label id="exL_logo"><input type="file" accept="image/*" id="exL_file" onChange="$_pImg(this)"></label><span id="btn_switch" class="numbtn" onClick="$_BTN_reverse()">左右<br>入替</span></div>';
+	$d += '<div class="space"><label id="exL_logo"><input type="file" accept="image/*" id="exL_file" onChange="$_pImg(this)"></label><span id="btn_switch" class="numbtn" onClick="$_BTN_reverse()">左右入替</span></div>';
 	$d += '<div class="right"><div class="team"><select class="name saveobj" id="player_name_r"><option class="def">-リストを読み込んでください-</option></select><select class="team saveobj" id="player_team_r" onChange="$_setTeam(this)">'+$t+'</select></div><input type="number" value="0" class="num saveobj" id="player_score_r" maxlength="1" min="0" max="9"><span id="up_R" class="numbtn nubup">+</span><span id="down_R" class="numbtn nubclr">0</span></div>';
 	$d += '</div>';
 	$d += '<div class="gs"><input type="text" class="csv saveobj" id="csvmaster" value="" placeholder="googleスプレッドシートID"><input type="button" class="submit" value="SET" onClick="$_BTN_getPL()"></div>';
