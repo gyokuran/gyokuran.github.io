531e0a9e6fb802994e9b3b5c6ca2a276f384ceaf
y.sato <y.sato@extia.jp>
2021-03-03 14:51:05 +0900

    Update bm3.js

--- bmtt/bm3.js
+++ bmtt/bm3.js
@@ -87,8 +87,8 @@ $css += '#exL #btn_switch { width:auto; height:auto; padding:4px; bottom:-26px; 
 
 	var $newWin = window.open('', 'スコアボードオーバーレイ', 'width=640,height=150,scrollbars=no');
 	$newWin.document.open();
-	$newWin.d.write('<html><head><title>スコアボードオーバーレイ</title><style>' + $css + '</style></head><body></body></html>');
-	$newWin.d.write($d);
+	$newWin.document.write('<html><head><title>スコアボードオーバーレイ</title><style>' + $css + '</style></head><body></body></html>');
+	$newWin.document.write($d);
 
 })(document);
 const $_pImg = function(obj){
